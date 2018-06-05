82adb57ca37205bb5ba591f740742009f41388b1
Nathan Lamb <nathanlamb022@gmail.com>
2018-06-05 16:31:27 +0200

    Clean up App.tsx and add Home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,14 @@
 import * as React from 'react';
 import './App.css';
+import Home from './components/Home'
+
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="wrap">
-        <div className="top-menu">
-          <nav className="ink-navigation ink-grid">
-            <ul className="menu horizontal black">
-              <li className="header active">
-                <a href="#">Home</a>
-              </li>
-              <li>
-                <a href="#">Inventory</a>
-              </li>
-              <li>
-                <a href="#">Patterns</a>
-              </li>
-              <li>
-                <a href="#">WIPs</a>
-              </li>
-            </ul>
-          </nav>
+        <div>
+          <Home />
         </div>
-        <div className="example">
-          <div className="ink-grid">
-            FO Gallery
-          </div>
-        </div>
-      </div>
     );
   }
 }
