4d1c5b365fc4bd6873d0f4be812d49980b62257b
Nathan Lamb <nathanlamb022@gmail.com>
2018-06-05 17:32:50 +0200

    Add Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import * as React from 'react';
-import './App.css';
-import Home from './components/Home'
+import { HashRouter as Router, Route } from "react-router-dom";
 
+import './App.css';
+
+import Home from './components/Home'
+import Inventory from './components/Inventory'
 
 class App extends React.Component {
   public render() {
     return (
-        <div>
-          <Home />
-        </div>
+        <Router>
+          <div>
+            <Route path="/" component={Home} />
+            <Route path="/inventory" component={Inventory} />
+            <Route path="/patterns" />
+            <Route path="/wips" />
+          </div>
+        </Router>
     );
   }
 }
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,14 +1,23 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
       <div>
         <nav className="ink-navigation">
           <ul className="menu horizontal black">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Inventory</a></li>
-            <li><a href="#">Patterns</a></li>
-            <li><a href="#">WIPs</a></li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/inventory">Inventory</Link>
+            </li>
+            <li>
+              <Link to="/patterns">Patterns</Link>
+            </li>
+            <li>
+              <Link to="/wips">WIPs</Link>
+            </li>
           </ul>
         </nav>
       </div>
