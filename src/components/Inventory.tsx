b757c76dd2a73b404db18093a1d7f711853df4a6
Nathan Lamb <nathanlamb022@gmail.com>
2018-06-05 18:54:55 +0200

    Remove Title to separate component

--- src/components/Inventory.tsx
+++ src/components/Inventory.tsx
@@ -3,9 +3,7 @@ import * as React from 'react';
 const Inventory = () => {
   return (
       <div className="ink-grid">
-        <div className="column-group">
-          <h1 className="fw-700 align-right title">Inventory</h1>
-        </div>
+        Invvv
       </div>
   )
 };
