fe9b707a864b160f4d9e17ff06e8d726bdd9df88
Nathan Lamb <nathanlamb022@gmail.com>
2018-06-05 18:56:12 +0200

    Change Home function to class

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,27 +1,29 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 
-const Home = () => {
-  return (
-      <div>
-        <nav className="ink-navigation">
-          <ul className="menu horizontal black">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/inventory">Inventory</Link>
-            </li>
-            <li>
-              <Link to="/patterns">Patterns</Link>
-            </li>
-            <li>
-              <Link to="/wips">WIPs</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-  )
-};
+class Home extends React.Component {
+  render() {
+    return (
+        <div>
+          <nav className="ink-navigation">
+            <ul className="menu horizontal black">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/inventory">Inventory</Link>
+              </li>
+              <li>
+                <Link to="/patterns">Patterns</Link>
+              </li>
+              <li>
+                <Link to="/wips">WIPs</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+    )
+  }
+}
 
 export default Home
