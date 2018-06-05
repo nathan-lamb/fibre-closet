747c57221380e55135d155c326981003eae8cd1a
Nathan Lamb <nathanlamb022@gmail.com>
2018-06-05 16:57:27 +0200

    Add menubar to Home component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,7 +3,14 @@ import * as React from 'react';
 const Home = () => {
   return (
       <div>
-
+        <nav className="ink-navigation">
+          <ul className="menu horizontal black">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Inventory</a></li>
+            <li><a href="#">Patterns</a></li>
+            <li><a href="#">WIPs</a></li>
+          </ul>
+        </nav>
       </div>
   )
 };
