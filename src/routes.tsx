990bba663b133419c68c41876a041244a6d4699a
Nathan Lamb <nathanlamb022@gmail.com>
2018-06-06 16:10:18 +0200

    install sqlite3 and knex

--- src/routes.tsx
+++ src/routes.tsx
@@ -11,27 +11,26 @@ const routes = [
     exact: true,
     object: Home,
     path: '/',
-    title: () => <h1 className="fw-700 align-right title">Home</h1>,
+    title: () => <h1 className="fw-700 align-right title">Home</h1>
   },
   {
     component: <div>Inventory</div>,
     object: Inventory,
     path: '/inventory',
-    title: () => <h1 className="fw-700 align-right title">Inventory</h1>,
-
+    title: () => <h1 className="fw-700 align-right title">Inventory</h1>
 },
   {
     component: <div>Patterns</div>,
     object: Patterns,
     path: '/patterns',
-    title: () => <h1 className="fw-700 align-right title">Patterns</h1>,
-},
+    title: () => <h1 className="fw-700 align-right title">Patterns</h1>
+  },
   {
     component: <div>WIPs</div>,
     object: Wips,
     path: '/wips',
-    title: () => <h1 className="fw-700 align-right title">WIPs</h1>,
-},
+    title: () => <h1 className="fw-700 align-right title">WIPs</h1>
+  },
 ];
 
 export default routes
