3b77f24b69b935c18c48b04fc9e158d1ec9adbd6
Nathan Lamb <nathanlamb022@gmail.com>
2018-06-09 19:17:44 +0200

    Add initial migration

--- knexfile.js
+++ knexfile.js
@@ -6,10 +6,9 @@ module.exports = {
     client: 'sqlite3',
     connection: {
       filename: './dev.sqlite3'
-    }
+    },
+    useNullAsDefault: true,
   },
-
-  useNullAsDefault: true,
 
   staging: {
     client: 'postgresql',
