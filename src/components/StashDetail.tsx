c98e3e377ac018a0a884a162253a13d953c1dd91
Nathan Lamb <nathanlamb022@gmail.com>
2018-06-10 10:31:59 +0200

    Add StashDetail component basic template

--- src/components/StashDetail.tsx
+++ src/components/StashDetail.tsx
@@ -2,7 +2,20 @@ import * as React from 'react'
 
 const StashDetail = () => {
   return (
-      <div>Stash Detail</div>
+      <div className="column-group gutters">
+        <div className="all-50">
+          Image
+        </div>
+        <div className="all-50">
+          <p>Yarn: </p>
+          <p>Brand: </p>
+          <p>Quantity: </p>
+          <p>Colourway: </p>
+          <p>Nearest Colour: </p>
+          <p>Weight: </p>
+          <p>Fibre Content: </p>
+        </div>
+      </div>
   )
 };
 
