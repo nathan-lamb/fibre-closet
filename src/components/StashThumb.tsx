ecbe7c1fc57902704969411a715698b4e2b9d68c
Nathan Lamb <nathanlamb022@gmail.com>
2018-06-10 10:31:34 +0200

    add styling

--- src/components/StashThumb.tsx
+++ src/components/StashThumb.tsx
@@ -2,9 +2,11 @@ import * as React from 'react'
 
 const StashThumb = () => {
   return (
-      <div className="thumbnail all-20">
-        This is a thumb
-      </div>
+      <a>
+        <div className="thumbnail all-20">
+          This is a thumb
+        </div>
+      </a>
   )
 };
 
